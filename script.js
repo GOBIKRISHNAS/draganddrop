a2a0b5ef50616f65b8142ac08c01e107c12ca36d
gobikrishnas <nivas.gobikrishna@gmail.com>
2021-04-08 13:57:58 +0530

    improvised

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
 const draggables = document.querySelectorAll('.draggable')
-const containers = document.querySelectorAll('.container')
+const containers = document.querySelectorAll('.place')
 
 draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-      draggable.classList.add('dragging')
+    draggable.addEventListener('dragstart', (e) => {
+      draggable.classList.add('dragging');
+      e.effectAllowed = "copyMove";
     })
   
-    draggable.addEventListener('dragend', () => {
+    draggable.addEventListener('dragend', (e) => {
       draggable.classList.remove('dragging')
     })
+
   })
 
-  containers.forEach(container => {
-    container.addEventListener('dragover', e => {
-      e.preventDefault()
-      const afterElement = getDragAfterElement(container, e.clientX)
-      const draggable = document.querySelector('.dragging')
-      if (afterElement == null) {
-        container.appendChild(draggable)
-      } else {
-        container.insertBefore(draggable, afterElement)
-      }
-    })
+containers.forEach(container => {
+  container.addEventListener('dragover', (e) => {
+    e.preventDefault();
   })
-  
-  function getDragAfterElement(container, x) {
-    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
-  
-    return draggableElements.reduce((closest, child) => {
-      const box = child.getBoundingClientRect()
-      const offset = x - box.left - box.width / 2
-      if (offset < 0 && offset > closest.offset) {
-        return { offset: offset, element: child }
-      } else {
-        return closest
-      }
-    }, { offset: Number.NEGATIVE_INFINITY }).element
-  }
+
+  container.addEventListener('drop', (e) => {
+    e.preventDefault();
+   var nodeCopy = document.getElementsByClassName('dragging')[0].cloneNode(true);
+   nodeCopy.id = "newId";
+   nodeCopy.draggable = "false";
+   console.log(nodeCopy)
+   var width = e.target.offsetWidth
+   e.target.style.width = width/2
+   console.log(e.target.offsetWidth)
+   console.log(e.target.getBoundingClientRect())
+   e.target.appendChild(nodeCopy);
+
+   
+
+    // Get mouse position relative to drop target
+    var dropPositionX = e.pageX - $(this).offset().left;
+    var dropPositionY = e.pageY - $(this).offset().top;
+
+
+    // Get mouse offset relative to dragged item
+    var dragItemOffsetX = e.offsetX;
+    var dragItemOffsetY = e.offsetY;
+
+    // Get position of dragged item relative to drop target
+    var dragItemPositionX = dropPositionX-dragItemOffsetX;
+    var dragItemPositionY = dropPositionY-dragItemOffsetY;
+  })
+})
+
+ 
